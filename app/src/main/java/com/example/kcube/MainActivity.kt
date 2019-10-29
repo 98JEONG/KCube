7c1e5f039fbabbe019ee69c4bc6b440b6f6c9b5c
SEJEONG_12 <mozzi981231@gmail.com>
2019-10-29 13:39:25 +0900

    2

--- app/src/main/java/com/example/kcube/MainActivity.kt
+++ app/src/main/java/com/example/kcube/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.kcube
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
@@ -10,5 +11,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Toast.makeText(this,"HELLO",Toast.LENGTH_SHORT).show()
+        Log.d("돼나","ehlsmsrjsrk")
     }
 }
